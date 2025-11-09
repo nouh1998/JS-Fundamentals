ef21aa1ae45a9a7e5e206a703a287d3aa8043062
nouh1998 <elattarnouh@gmail.com>
2025-11-09 23:52:51 +0100

    dd

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+// 7-multi_c.js
+
+const args = process.argv.slice(2);
+const x = parseInt(args[0]);
+
+if (!x || x <= 0) {
+    console.log("Missing number of occurrences");
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}
