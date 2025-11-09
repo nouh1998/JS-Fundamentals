652c2dc6d25bcb20ad4b198b3a34ed4e27cfcfbb
nouh1998 <elattarnouh@gmail.com>
2025-11-09 23:58:06 +0100

    ss

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+// 9-add.js
+
+const args = process.argv.slice(2);
+
+function add(a, b) {
+    return a + b;
+}
+
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+
+console.log(add(num1, num2));
