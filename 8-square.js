a52c8d2a936c6288c652bd27aad9eff246a94e78
nouh1998 <elattarnouh@gmail.com>
2025-11-09 23:55:59 +0100

    ddd

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+// 8-square.js
+
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (!args[0] || isNaN(size)) {
+    console.log("Missing size");
+} else if (size > 0) {
+    for (let i = 0; i < size; i++) {
+        console.log("X".repeat(size));
+    }
+}
