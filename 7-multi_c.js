e82499778b575215ee4bc5747c3f61399d6deb0c
nouh1998 <elattarnouh@gmail.com>
2025-11-09 23:54:31 +0100

    Fix input validation for occurrences

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 const x = parseInt(args[0]);
 
-if (!x || x <= 0) {
+if (!args[0] || isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
     for (let i = 0; i < x; i++) {
